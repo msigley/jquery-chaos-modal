81745b013fca4f2e0d0fdc78d10a8460aaa6eafe
Jacob Newmister <newmisterJ@gmail.com>
2015-10-02 10:15:28 -0400

    Added events for pre-processing of modal content and the interrupting of pre-processing upon modal close. Improvements to image and iframe content handling.

--- jquery.modal.js
+++ jquery.modal.js
@@ -2,22 +2,64 @@
  * jQuery Chaos Modal
  * By Matthew Sigley
  * Based on concept work by Kevin Liew - http://www.queness.com/post/77/simple-jquery-modal-window-tutorial
- * Version 1.3.10
+ * Version 1.4
  */
 
 (function( $ ) {
+
+	//Bowser IE version detection
+	function getIEVersion() {
+		var ua = typeof navigator !== 'undefined' ? navigator.userAgent : '';
+		if( /msie|trident/i.test(ua) ) {
+			var match = ua.match(/(?:msie |rv:)(\d+(\.\d+)?)/i);
+    		return (match && match.length > 1 && match[1]) || '';
+    	}
+    	return '';
+    }
+
+	var windowElement = $(window),
+		 IEVersion = '';
+	
+	if( bowser.msie )
+		IEVersion = bowser.version;
+	else
+		IEVersion = getIEVersion();
+	
+	//Detects images that are not visible
+	var isVisible = function( element ) {
+		if( element.offsetParent === null )
+			return false;
+		if( IEVersion < 11 ) {
+			var computedDisplay = '';
+			if( typeof getComputedStyle != 'undefined' )
+				computedDisplay = window.getComputedStyle(element).display;
+			else if( typeof element.currentStyle != 'undefined' )
+				computedDisplay = element.currentStyle['display'];
+			if( 'none' == computedDisplay || 'fixed' == computedDisplay || 'absolute' == computedDisplay )
+				return false;
+		}
+		return true;
+	};
+
+
 	//Global variables for generating modal box clones
-	$.chaosModalCurrent = null;
+	$.chaosModalLoading  = null;
+	$.modalMask          = null;
+	$.chaosModalCurrent  = null;
 	$.chaosModalMaxWidth = 0;
-	$.chaosModalIndex = 0;
-	$.chaosModalImagesLoaded = false;
+	$.chaosModalIndex    = 0;
+	$.chaosModalImagesLoaded  = false;
 	$.chaosModalIframesLoaded = false;
 	
 	$.fn.openModal = function( options ) {
+		//If modal already open, return
+		if($.chaosModalCurrent != null){
+			return;
+		}
 		//Default options
-		var defaults = { maskClose: true, maxWidth: 960, closeLink: true, printLink: false, alwaysAtTop: false };
-		options = $.extend({}, defaults, options);
-		
+		var defaults = { maxWidth: 960, closeLink: true, printLink: false, alwaysAtTop: false, preprocessing: false };
+		    options  = $.extend({}, defaults, options);
+
 		//Lazy Load iFrame content
 		function iframeDataSrc() {
 			var thisElement = $(this),
@@ -26,29 +68,22 @@
 				thisElement.attr('src', iframeDataSrc).attr('data-src', '');
 		}
 		this.find('iframe[src=""]').each(iframeDataSrc);
-		
+
 		//Clone modal content
-		var clone = this.clone(true),
-			bodyElement = $('body'),
-			htmlElement =  $('html'),
-			documentElement = $(document),
-			windowElement = $(window),
-			maskClose = options['maskClose'],
-			closeLink = options['closeLink'],
-			printLink = options['printLink'],
-			alwaysAtTop = options['alwaysAtTop'];
-			
+		var clone           = this.clone(true),
+			  bodyElement     = $('body'),
+			  htmlElement     = $('html'),
+			  documentElement = $(document),
+			  windowElement   = $(window),
+			  closeLink       = options['closeLink'],
+			  printLink       = options['printLink'],
+			  alwaysAtTop     = options['alwaysAtTop'];
 		//Update $.chaosModalMaxWidth
 		$.chaosModalMaxWidth = options['maxWidth'];
 		
 		//Update $.chaosModalCurrent
 		$.chaosModalCurrent = clone;
 		clone.attr({id: 'chaos-current-modal'});
-		
-		//Write the mask div
-		var modalMask = $('<div id="chaos-modal-mask" class="chaos-modal-mask"></div>');
-		modalMask.css({'position': 'absolute', 'z-index': '9000', 'background-color': '#000', 'display': 'none', 'top': '0', 'left': '0'});
-		modalMask.prependTo(bodyElement);
 		
 		//Write print link if none exist
 		if(clone.find('.print-link').length == 0 && printLink) {
@@ -62,85 +97,96 @@
 			clone.children('.close-link').css({'float': 'right', 'margin': '5px'});
 		}
                
-        //Set the popup window css
-        clone.css({'display': 'block', 'position': 'absolute', 'background': '#fff', 'z-index': '9001', 'left': '-10000px', 'margin': '0', 'padding': '0'});
-        clone.prependTo(bodyElement);
-        
-        var showModal = function() {
-        	//Lock popup window width
-	       	if(clone.width() > $.chaosModalMaxWidth){
-	       		clone.width($.chaosModalMaxWidth);
-	       	} else {
-	       		clone.width(clone.width());
-	       	}
+    //Set the popup window css
+    clone.css({'display': 'block', 'position': 'absolute', 'background': '#fff', 'z-index': '9002', 'left': '-10000px', 'margin': '0', 'padding': '0'});
+    clone.prependTo(bodyElement);
+     
+    var showModal = function() {
+			//Lock popup window width
+	    if(clone.width() > $.chaosModalMaxWidth){
+	      clone.width($.chaosModalMaxWidth);
+	    } else {
+	      clone.width(clone.width());
+	    }
 	       	
-	       	//Fix content widths
-	       	clone.find('img').css('max-width', '100%'); 
+	    //Fix content widths
+	    clone.find('img').css('max-width', '100%'); 
 	       	
-	       	clone.hide();
+	    clone.hide();
 	       	
-	     	//Calculate the modal mask size and popup window position
-	     	clone.resizeModal( alwaysAtTop );
+			//Calculate the modal mask size and popup window position
+			clone.resizeModal( alwaysAtTop );
 	     	
-	     	//Mask transition effect    
-	        modalMask.fadeIn(1000);   
-	        modalMask.fadeTo("slow",0.8);
-	     	
-	        //transition effect
-	        clone.fadeIn(2000, function() {
-	        	//Bind the window resize event
-	       		windowElement.bind('resize', {'alwaysAtTop': alwaysAtTop}, resizeCurrentModal);
-	        });
+	    //transition effect
+			$.modalMask.stop();
+	    clone.fadeIn(2000, function() {
+	      //Bind the window resize event
+				clone.resizeModal( alwaysAtTop );
+	      windowElement.bind('resize', {'alwaysAtTop': alwaysAtTop}, resizeCurrentModal);
+				$.fn.closeLoading();
+	    });
 	        
-	        //Bind the print link events if any close links exist
-	        if(clone.find('.print-link').length > 0) {
-	        	clone.find('.print-link').bind('click', printCurrentModal);
-	        }
+	    //Bind the print link events if any close links exist
+	    if(clone.find('.print-link').length > 0) {
+	      clone.find('.print-link').bind('click', printCurrentModal);
+	    }
 	        
-	        //Bind the close link events if any close links exist
-	        if(clone.find('.close-link').length > 0) {
-	        	clone.find('.close-link').bind('click', closeCurrentModal);
-	         }
+	    //Bind the close link events if any close links exist
+	    if(clone.find('.close-link').length > 0) {
+	      clone.find('.close-link').bind('click', closeCurrentModal);
+	      }
 	        
-	        //Bind close event to mask click and when the ESC key is pressed
-			if(maskClose) {
-				modalMask.on('click', closeCurrentModal);
-				documentElement.on('keyup', closeOnESC);
-			}
-        };
+    };
         
-        //Show modal after all images and iframes have loaded
-        var lastImage = clone.find('img').last(),
-        	lastIframe = clone.find('iframe').last();
-        $.chaosModalImagesLoaded = (lastImage.length) ? false : true;
-        $.chaosModalIframesLoaded = (lastIframe.length) ? false : true;
-        
-        if($.chaosModalImagesLoaded && $.chaosModalIframesLoaded)
-        	showModal();
-        else {
-        	lastImage.on('load', function() {
-        		$.chaosModalImagesLoaded = true;
-        		if($.chaosModalImagesLoaded && $.chaosModalIframesLoaded)
-        			showModal();
-        	});
-        	lastIframe.on('load', function() {
-        		$.chaosModalIframesLoaded = true;
-        		if($.chaosModalImagesLoaded && $.chaosModalIframesLoaded)
-        			showModal();
-        	});
-        }
-        
-        clone.show();
-        
-        return this;
-	};
-	
-	$.fn.closeModal = function() {
-		//Clear $.chaosModalCurrent
-		$.chaosModalCurrent = null;
+		var isImageNotLoaded = function( index, img ) {
+			if (!isVisible(img))
+				return false;
+			if (img.complete)
+				return false;
+			if (typeof img.naturalWidth == "undefined" || img.naturalWidth != 0)
+				return false;
+			return true;
+		};
 		
+		var isIframeNotLoaded = function( index, iframe ) {
+			if (!isVisible(iframe))
+		    	return false;
+				if ('complete' == iframe.contentDocument.readyState)
+		    	return false;
+			return true;
+		};
+
+    //Show modal after all images and iframes have loaded
+    var images  = clone.find('img').filter(isImageNotLoaded),
+        iframes = clone.find('iframe').filter(isIframeNotLoaded);
+				$.imagesToLoad  = images.length;
+				$.iframesToLoad = iframes.length;
+
+    if(!$.imagesToLoad && !$.iframesToLoad)
+      showModal();
+    else {
+
+      images.on('load', function() {
+        $.imagesToLoad -= 1;
+        if(!$.imagesToLoad && !$.iframesToLoad)
+        	showModal();
+      });
+      iframes.on('load', function() {
+        $.iframesToLoad -= 1;
+        if(!$.imagesToLoad && !$.iframesToLoad)
+        	showModal();
+      });
+    }
+        
+    clone.show();
+        
+    return this;
+	};
+	$.fn.closeModal = function() {
 		//Unbind the window resize event
 		$(window).unbind('resize', resizeCurrentModal);
+		//Clear $.chaosModalCurrent
+		$.chaosModalCurrent = null;
 		
 		//Unbind the close link events if any close links exist
         if(this.find('.close-link').length > 0) {
@@ -152,18 +198,19 @@
 		
 		//Remove the mask div
 		$('#chaos-modal-mask').hide().remove();
+		$.fn.closeLoading();
 		
 		return this;
 	};
 	
 	$.fn.resizeModal = function( alwaysAtTop ) {
 		var documentElement = $(document),
-			windowElement = $(window),
-			htmlElement = $('html'),
-			maskHeight = documentElement.height(), //Get the screen height and width
-			maskWidth = htmlElement.width(),
-			winH = windowElement.height(), //Get the window height and width
-        	winW = windowElement.width();
+			  windowElement   = $(window),
+			  htmlElement     = $('html'),
+			  maskHeight      = documentElement.height(), //Get the screen height and width
+			  maskWidth       = htmlElement.width(),
+			  winH            = windowElement.height(), //Get the window height and width
+        winW            = windowElement.width();
         
         //Check for invalid mask dimensions
         if(maskHeight < this.height()) { maskHeight = this.height(); }
@@ -173,8 +220,8 @@
         $('#chaos-modal-mask').css({'width':maskWidth,'height':maskHeight});
                 
         //Calculate popup window position
-        var modalTop = winH/2-this.height()/2,
-        	modalLeft = winW/2-this.width()/2;
+        var modalTop  = winH/2-this.height()/2,
+        	   modalLeft = winW/2-this.width()/2;
         
         //Check for invalid window positions
         if (modalTop < 0) { modalTop = 0; }
@@ -208,7 +255,19 @@
 	
 	function closeCurrentModal(e) {
 		e.preventDefault(); //Prevents browser from following links
-		$.chaosModalCurrent.closeModal();
+		//Velocity control
+		if(Date.now() - $.openTime < 500){
+			return false;
+		}
+		$(document).off('.chaosmodal');
+		if($.chaosModalCurrent != null)
+			$.chaosModalCurrent.closeModal();
+		else{
+			$(window).trigger('chaos-modal-preprocess-interrupt');
+			$('#chaos-modal-mask').hide().remove();
+			$.fn.closeLoading();
+		}
+		$.modalMask = null;
 	}
 	
 	function printCurrentModal(e) {
@@ -217,10 +276,90 @@
 	}
 	function closeOnESC(e) {
 		if( e.which == 27 ) {
-			$(this).off('keyup', closeOnESC);
 			closeCurrentModal(e);
 		}
 	}
+
+	//Function shows the loading screen (modalMask and loading gif)
+	$.fn.showLoading = function(){
+		//Sets open time for velocity control
+		$.openTime = Date.now();
+		if($.modalMask != null){
+			return;
+		}
+		var bodyElement   = $('body'),
+		    windowElement = $(window),
+		    winH          = windowElement.height(), //Get the window height and width
+		    winW          = windowElement.width();
+			  maskClose     = true;
+		//Create mask div
+		$.modalMask = $('<div id="chaos-modal-mask" class="chaos-modal-mask"></div>');
+		$.modalMask.css({'position': 'absolute', 'z-index': '9000', 'background-color': '#000', 'display': 'none', 'top': '0', 'left': '0'});
+		
+		$.modalMask.prependTo(bodyElement);
+
+		//Mask transition effect    
+	   $.modalMask.fadeIn(500);   
+	   $.modalMask.fadeTo("fast",0.8);
+
+		var documentElement = $(document),
+			  windowElement   = $(window),
+			  htmlElement     = $('html'),
+			  maskHeight      = documentElement.height(), //Get the screen height and width
+			  maskWidth       = htmlElement.width();
+		
+		//Bind close event to mask click and when the ESC key is pressed
+		if(maskClose) {
+			$.modalMask.on('click', closeCurrentModal);
+			documentElement.on('keyup.chaosmodal', closeOnESC);
+		}
+
+		//Check for invalid mask dimensions
+    if(maskHeight < this.height()) { maskHeight = this.height(); }
+    if(maskWidth < winW) { maskWidth = winW; }
+        
+    //Set height and width to mask to fill up the whole screen
+    $('#chaos-modal-mask').css({'width':maskWidth,'height':maskHeight});
+
+		//Sets positioning for the loading bar
+		
+		$('body').append('<div id="modal-loading" style="position: fixed; z-index: 9001; color: #fff;">Loading</div>');
+		
+		//Center Loading
+		posx = (winW / 2) - parseInt($('#modal-loading').css('width')) / 2;
+		posy = (winH / 2) - parseInt($('#modal-loading').css('height')) / 2;
+		
+		$.chaosModalLoading = $('#modal-loading');
+		$.chaosModalLoading.css('top', posy);
+		$.chaosModalLoading.css('left', posx);
+		var alwaysAtTop = true;
+  	windowElement.bind('resize', {'alwaysAtTop': alwaysAtTop}, resizeLoading);	
+	}
+	function resizeLoading(){
+		var documentElement = $(document),
+			  windowElement   = $(window),
+			  htmlElement     = $('html'),
+			  maskHeight      = documentElement.height(), //Get the screen height and width
+			  maskWidth       = htmlElement.width(),
+			  winH            = windowElement.height(), //Get the window height and width
+        winW            = windowElement.width();
+
+		$('#chaos-modal-mask').css({'width':maskWidth,'height':maskHeight});
+
+		//Center Loading
+		posx = (winW / 2) - parseInt($('#modal-loading').css('width')) / 2;
+		posy = (winH / 2) - parseInt($('#modal-loading').css('height')) / 2;
+		
+		$.chaosModalLoading = $('#modal-loading');
+		$.chaosModalLoading.css('top', posy);
+		$.chaosModalLoading.css('left', posx);
+	}
+
+	//Closes the loading bar
+	$.fn.closeLoading = function(){
+		$.chaosModalLoading.remove();
+	}
+	
 })( jQuery );
 
 /* Create modal box clones and click events */
@@ -237,7 +376,7 @@ jQuery(document).ready(function($){
 	});
 	
 	//Attach click event to links
-	$('a.chaos-modal-link').one('click', processModalLink);
+	$('a.chaos-modal-link').on('click', processModalLink);
 	
 	//Lazy Load iFrame content
 	function iframeDataSrc() {
@@ -254,15 +393,15 @@ jQuery(document).ready(function($){
 	
 	function processModalLink(e) {
 		var thisElement = $(this);
-		
+	
 		e.preventDefault(); //Prevents browser from following links
 		thisElement.off('click', processModalLink); //Remove content processing event. It should only be run once per link.
 		
-		var modalContentId = thisElement.data('chaos-modal-box-id'),
-			modalContent = false,
-			modalContentClone = false,
-			imageRegex = /\.(jpeg|jpg|gif|png|bmp|wbmp)$/i,
-			imageUrl = false;
+		var modalContentId    = thisElement.data('chaos-modal-box-id'),
+				modalContent      = false,
+				modalContentClone = false,
+				imageRegex        = /\.(jpeg|jpg|gif|png|bmp|wbmp)$/i,
+				imageUrl          = false;
 		
 		if( modalContentId )
 			modalContent = $('#'+modalContentId).first();
@@ -297,7 +436,6 @@ jQuery(document).ready(function($){
 				}
 			}
 		}
-		
 		//Setup openModal event if content is available
 		if( modalContentClone ) {
 			//Remove original content element to avoid id conflicts
@@ -314,15 +452,18 @@ jQuery(document).ready(function($){
 			
 			//Append hidden modal content to body
 			modalContentClone.appendTo('body');
-			
 			thisElement.click(function() {
+				$.fn.showLoading();
+				preprocess = thisElement.attr('data-preprocess');
 				var thisId = $(this).attr('id'),
 					indexRegex = /chaos-modal-link-(\d+)/i,
 					chaosModalIndex = thisId.match(indexRegex);
 				if( chaosModalIndex[1] ) {
 					var modalBox = $('#chaos-modal-box-'+chaosModalIndex[1]);
-					if( modalBox.length == 1 ) {
+					if( modalBox.length == 1 && preprocess != 'true' ) {
 						modalBox.openModal();
+					} else if (modalBox.length == 1 && preprocess == 'true'){
+						thisElement.trigger("chaos-modal-preprocess", modalBox);
 					}
 				}
 				return false;
@@ -332,5 +473,11 @@ jQuery(document).ready(function($){
 			$.chaosModalIndex++;
 		}
 		return false;
+	}
+	//Polyfill for Date.now()
+	if(!Date.now){
+			Date.now = function now(){
+				return new Date().getTime();
+			};
 	}
 });
